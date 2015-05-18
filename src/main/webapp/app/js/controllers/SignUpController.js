419c18a178d06a8e40a6d1fec2afd564c0ce3a5a
Simon THOURY <simon.thoury@gmail.com>
2015-05-18 17:54:47 +0200

    Formulaires d'inscription et de connexion

--- src/main/webapp/app/js/controllers/SignUpController.js
+++ src/main/webapp/app/js/controllers/SignUpController.js
@@ -2,7 +2,61 @@
  * Contrôleur de la page d'enregistrement de compte
  */
 
-angular.module("carnetAdresses").controller("SignUpController", function($scope, $location) {
+angular.module("carnetAdresses").controller("SignUpController", function($scope, $location, SignUpService, ModalService) {
+	
+	$scope.profil = {};
+
+	// Fonction de sauvegarde du formulaire
+	$scope.saveProfil = function () {
+		SignUpService.saveProfil($scope.profil) 
+		.then(
+			function(result) {			
+				$scope.saveOk();					
+				angular.extend($scope.profil, {
+					firstName: '', lastName: '',
+					email: '', login: '',
+					password: '', telephone: ''
+				});						
+			},
+			function(error) {
+				switch(error.status) {
+				case 400 :
+					$scope.saveExists();
+					break;
+				default :
+					$scope.saveNoOk();
+				}
+			}
+		);
+	};
+	
+	$scope.saveOk = function () {
+		ModalService.openModal(
+			"Validation de l'inscription",
+			"Félicitations, votre inscription a bien été prise en compte !",
+			"OK"
+		).result.then(
+			$scope.goToSignIn,
+			$scope.goToSignIn	
+		);
+	};	
+	
+	$scope.saveNoOk = function () {
+		ModalService.openModal(
+			"Échec de l'inscription",
+			"Aïe, l'inscription n'a pas fonctionné !",
+			"OK"
+		);
+	};	
+	
+	$scope.saveExists = function () {
+		ModalService.openModal(
+			"Échec de l'inscription",
+			"L'adresse mail ou le login que vous avez choisi est déjà utilisé pour un compte !",
+			"OK"
+		);
+	};		
+		
 	
 	$scope.goToSignIn = function() {
 		$location.path("/signin");
--- src/main/webapp/app/js/services/SignUpService.js
+++ src/main/webapp/app/js/services/SignUpService.js
@@ -5,7 +5,7 @@
 angular.module("carnetAdresses").factory("SignUpService", function ($http) {
 	return {
 		saveProfil: function (profil) {
-			return $http.post("/", profil)
+			return $http.post("/CarnetAdresses/rest/user", profil)
 		}
 	}
 });
